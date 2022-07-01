619d73aa3c14e976b18681d44a6790fa0bd53b3b
Alex-Miarowski <alex.miarowski@gmail.com>
2022-07-01 15:26:57 +0200

    Working version 0.1

--- exchange-rates-api/src/main/java/com/miarowski/exchangeratesapi/ExchangeController.java
+++ exchange-rates-api/src/main/java/com/miarowski/exchangeratesapi/ExchangeController.java
@@ -3,7 +3,9 @@ package com.miarowski.exchangeratesapi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
@@ -13,18 +15,20 @@ public class ExchangeController {
     @Autowired
     ExchangeRepository exchangeRepository;
 
-    @GetMapping("/test")
-    public int test(){
-        return 1;
-    }
-
     @GetMapping("/all")
     public List<CurrencyPair> getAll(){
         return exchangeRepository.getAll();
     }
 
     @GetMapping("/by-date/{date}")
-    public List<CurrencyPair> getByDate(@PathVariable("date") LocalDate date){
-        return exchangeRepository.getByDate(date);
+    public List<CurrencyPair> getByDate(@PathVariable("date") String date){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return exchangeRepository.getByDate(LocalDate.parse(date, formatter));
+    }
+
+    @GetMapping("/pair-on-date/{date}/{firstCurrency}-{secondCurrency}")
+    public CurrencyPair getPairOnDate(@PathVariable("date") String date, @PathVariable("firstCurrency") String firstCurrency, @PathVariable("secondCurrency") String secondCurrency){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return exchangeRepository.getPairOnDate(LocalDate.parse(date, formatter), firstCurrency.toUpperCase(), secondCurrency.toUpperCase());
     }
 }
--- exchange-rates-api/src/main/java/com/miarowski/exchangeratesapi/ExchangeRepository.java
+++ exchange-rates-api/src/main/java/com/miarowski/exchangeratesapi/ExchangeRepository.java
@@ -7,25 +7,27 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class ExchangeRepository {
 
+    @Autowired
     JdbcTemplate jdbcTemplate;
 
     public List<CurrencyPair> getAll(){
-        return jdbcTemplate.query("SELECT date, first_currency, second_currency, exchange_rate FROM db",
+        return jdbcTemplate.query("SELECT date, first_currency, second_currency, exchange_rate FROM currencies",
                 BeanPropertyRowMapper.newInstance(CurrencyPair.class));
     }
 
     public List<CurrencyPair> getByDate(LocalDate date){
-        return jdbcTemplate.query("SELECT date, first_currency, second_currency, exchange_rate FROM db WHERE " +
-                "data = ?", BeanPropertyRowMapper.newInstance(CurrencyPair.class), date);
+        return jdbcTemplate.query("SELECT date, first_currency, second_currency, exchange_rate FROM currencies WHERE " +
+                "date = ?", BeanPropertyRowMapper.newInstance(CurrencyPair.class), date);
     }
 
     public CurrencyPair getPairOnDate(LocalDate date, String firstCurrency, String secondCurrency){
-        return jdbcTemplate.query("SELECT date, first_currency, second_currency, exchange_rate FROM db WHERE " +
-                "date = ? AND first_currency = ? AND second_currency = ?", new Object[] { date, firstCurrency, secondCurrency });
+        return jdbcTemplate.queryForObject("SELECT date, first_currency, second_currency, exchange_rate FROM currencies WHERE " +
+                "date = ? AND first_currency = ? AND second_currency = ?", BeanPropertyRowMapper.newInstance(CurrencyPair.class), date, firstCurrency, secondCurrency);
     }
 }
